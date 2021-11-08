7ff1fd752e2cd5417c9e5991a1e602b5e27bc14b
MaksymM92 <maksym.mostovyi.ua@gmail.com>
2021-11-08 21:10:46 +0400

    'adding heatmap demo'

---
+++ src/app/data.ts
@@ -0,0 +1,10 @@
+export const heatData: any = {
+  data: [
+    {lat: 48.37, lng: 31.16, count: 143},
+    {lat: 49.37, lng: 36.16, count: 133},
+    {lat: 43.32, lng: 37.13, count: 163},
+    {lat: 39.33, lng: 38.42, count: 143},
+    {lat: 42.31, lng: 34.65, count: 133},
+    {lat: 43.39, lng: 32.10, count: 163},
+  ]
+}
